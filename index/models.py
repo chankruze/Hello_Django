225045bae67eb87237c55c82b570cb7dda43a24d
chankruze <chankruze@gmail.com>
2019-04-25 20:11:37 +0530

    Add Models
    Signed-off-by: chankruze <chankruze@gmail.com>

--- index/models.py
+++ index/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    top_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class Webpage(models.Model):
+    # django > 2.0 takes 2 params : models.ForeignKey('Category', on_delete=models.PROTECT)
+    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
+    name = models.CharField(max_length=50, unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    # django > 2.0 takes 2 params : models.ForeignKey('Category', on_delete=models.PROTECT)
+    name = models.ForeignKey(Webpage, on_delete=models.PROTECT)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
